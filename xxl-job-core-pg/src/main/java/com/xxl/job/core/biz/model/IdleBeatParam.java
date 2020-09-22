9da1525509c06727defb389a892f5dc7f569f0a8
luyanwen <xue_2013@sina.com>
2020-09-22 13:58:27 +0800

    [+]修改pg依赖，修改实体id为long

---
+++ xxl-job-core-pg/src/main/java/com/xxl/job/core/biz/model/IdleBeatParam.java
@@ -0,0 +1,28 @@
+package com.xxl.job.core.biz.model;
+
+import java.io.Serializable;
+
+/**
+ * @author xuxueli 2020-04-11 22:27
+ */
+public class IdleBeatParam implements Serializable {
+    private static final long serialVersionUID = 42L;
+
+    public IdleBeatParam() {
+    }
+    public IdleBeatParam(Long jobId) {
+        this.jobId = jobId;
+    }
+
+    private Long jobId;
+
+
+    public Long getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(Long jobId) {
+        this.jobId = jobId;
+    }
+
+}
